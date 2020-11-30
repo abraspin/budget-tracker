7a5b02db80c4887b66fa9f828f0f4ca0cc309e3f
abraspin <abrahamspindel@gmail.com>
2020-11-30 11:56:44 -0600

    update mongoose connection to use atlasDB URI if available

--- server.js
+++ server.js
@@ -15,10 +15,17 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+// mongoose.connect("mongodb://localhost/budget", {
+//   useNewUrlParser: true,
+//   useFindAndModify: false,
+//   useUnifiedTopology: true,
+// });
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
-  useFindAndModify: false,
   useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
 });
 
 // routes
